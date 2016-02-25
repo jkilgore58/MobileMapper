3958ba297a2252005b2892e086639869ac81f36e
Jonathan Kilgore <jonathan.kilgore58@gmail.com>
2016-02-25 15:15:59 -0600

    final updates

--- MobileMapper/ViewController.swift
+++ MobileMapper/ViewController.swift
@@ -7,19 +7,77 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate {
 
+    @IBOutlet weak var mapView: MKMapView!
+    let mobileMakersAnnotation = MKPointAnnotation()
+    let locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
+        locationManager.requestWhenInUseAuthorization()
+        mapView.showsUserLocation = true
+        mapView.showsCompass = true
+        mapView.showsTraffic = true
+        
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+        
+        //Mobile Makers coordinates via GoogleMaps: 41.8937362(lat),-87.6375008(long)
+        
+        let latitude = 41.8937362
+        let longtitude = -87.6375008
+        
+        mobileMakersAnnotation.coordinate = CLLocationCoordinate2DMake(latitude, longtitude)
+        mobileMakersAnnotation.title = "Mobile Makers HQ"
+        mapView.addAnnotation(mobileMakersAnnotation)
+        
+        //let address = "Yosemite National Park"
+        dropPinForLocation("Yosemite National Park")
+        dropPinForLocation("Yellowstone National Park")
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
+    
+    func dropPinForLocation(address: String) {
+        let geocoder = CLGeocoder()
+        
+        geocoder.geocodeAddressString(address) { (placemarks:[CLPlacemark]?, error:NSError?) -> Void in
+            for placemark in placemarks!
+            {
+                let annotation = MKPointAnnotation()
+                annotation.coordinate = (placemark.location?.coordinate)!
+                annotation.title = placemark.name
+                self.mapView.addAnnotation(annotation)
+            }
+        }
+    }
+    
+    func mapView(mapView: MKMapView, viewForAnnotation annotation: MKAnnotation) -> MKAnnotationView? {
+        if annotation.isEqual(mapView.userLocation)
+        {
+            return nil
+        } else if annotation.isEqual(mobileMakersAnnotation) {
+        
+        let pin = MKAnnotationView(annotation: annotation, reuseIdentifier: nil)
+        pin.image = UIImage(named: "mobilemakers")
+        pin.canShowCallout = true
+        pin.rightCalloutAccessoryView = UIButton(type: .DetailDisclosure)
+        
+        return pin
+            
+        } else {
+            
+            let pin = MKPinAnnotationView(annotation: annotation, reuseIdentifier: nil)
+            pin.canShowCallout = true
+            pin.rightCalloutAccessoryView = UIButton(type: .DetailDisclosure)
+            
+            return pin
+        }
+    }
+    
+    func mapView(mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
+        mapView.setRegion(MKCoordinateRegionMake(view.annotation!.coordinate, MKCoordinateSpanMake(0.1, 0.1)), animated: true)
+    }
 
 }
 
